700d64f0dbd1d68100071f79f197c413bad0a6ec
Ihtisham-Khattak <ikpashteen1@gmail.com>
2023-08-02 18:01:30 +0500

    Form:Form added

--- pages/product/newProduct.js
+++ pages/product/newProduct.js
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from "react";
+import {
+  Row,
+  Card,
+  CardBody,
+  CardTitle,
+  Col,
+  Label,
+  Input,
+  FormText,
+  Button,
+  Form,
+  FormGroup,
+} from "reactstrap";
 
 const newProduct = () => {
   return (
-    <div>newProduct</div>
-  )
-}
+    <Row>
+      <Col>
+        <Card>
+          <CardTitle tag="h6" className="mt-3 p-3 font-bold white">
+            Add New Product
+          </CardTitle>
+          <CardBody>
+            <Form>
+              <FormGroup>
+                <Label for="exampleEmail">Email</Label>
+                <Input
+                  id="exampleEmail"
+                  name="email"
+                  placeholder="with a placeholder"
+                  type="email"
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="examplePassword">Password</Label>
+                <Input
+                  id="examplePassword"
+                  name="password"
+                  placeholder="password placeholder"
+                  type="password"
+                />
+              </FormGroup>
 
-export default newProduct
+              <FormGroup>
+                <Label for="exampleText">Text Area</Label>
+                <Input id="exampleText" name="text" type="textarea" />
+              </FormGroup>
+
+              <Button>Submit</Button>
+            </Form>
+          </CardBody>
+        </Card>
+      </Col>
+    </Row>
+  );
+};
+
+export default newProduct;
